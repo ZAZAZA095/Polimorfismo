9dfaae04c72b42a96ffd7240d1ecc13b713f8adb
ZAZAZA095 <a7513@palmore.edu.mx>
2025-02-27 11:55:22 -0600

    Polimorfismo

---
+++ Medidas metricas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class polimorfismo
+    {
+
+    
+        public class Valores
+        {
+            public static double yardasm = 1.09361;
+            public static double metrosy = 0.9144;
+
+        }
+        public double Convertir(double Yardas)
+        {
+            return Yardas * Valores.metrosy;
+
+        }
+        public double Convertir(double Yardas, double Centimetros)
+        {
+
+            return Yardas * Valores.metrosy * Centimetros;
+
+        }
+        public double Convertir(double Yardas, double Centimetros, double Milimetros)
+        {
+
+            return Yardas * Valores.metrosy * Centimetros * Milimetros;
+
+        }
+        public double Convertir(double Yardas, double Centimetros, double Milimetros, double Micrometro)
+        {
+
+            return Yardas * Valores.metrosy * Centimetros * Milimetros * Micrometro;
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Programa para comvertir Metros a Yardas y viceversa");
+
+            polimorfismo Prueba1 = new polimorfismo();
+
+            Console.WriteLine("Yardas a Metros: 4yd = " + Prueba1.Convertir(4));
+            Console.WriteLine("Yardas a Centimetros: 4yd = " + Prueba1.Convertir(4, 100));
+            Console.WriteLine("Yardas a Milimetros: 4yd = " + Prueba1.Convertir(4, 100, 10));
+            Console.WriteLine("Yardas a Micrometros: 4yd = " + Prueba1.Convertir(4, 100, 10, 1000));
+            Console.ReadLine();
+        }
+    }
+}
